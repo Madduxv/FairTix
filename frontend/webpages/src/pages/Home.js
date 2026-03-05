cd4973daa002eb1dcab0260348297b401c711a8d
paraselenite <skylarfrader@gmail.com>
2026-03-05 13:04:20 -0600

    WORKING LOGO RESIZE!!!!!!!

--- frontend/webpages/src/pages/Home.js
+++ frontend/webpages/src/pages/Home.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import UseDeviceSize from '../UseDeviceSize'
+import logo from '../logo.png';
 
 function Home() {
+   const [width, height] = UseDeviceSize();
    let navigate = useNavigate();
    return (
       <div>
          <br></br><br></br><br></br>
-         <h1>WELCOME TO FAIRTIX [skylar will put a logo here later]</h1>
+         <div> <img width={width} src={logo} alt="really cool logo, trust"/></div>
          <div><button onClick={() => { navigate("/login"); }}>Log in</button> <button onClick={() => { navigate("/signup"); }}>Sign up</button> <button>Continue as guest</button></div>
       </div>
    );
